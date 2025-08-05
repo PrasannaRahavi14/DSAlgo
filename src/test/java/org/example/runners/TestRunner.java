b82c42c987ed38635d2c583c842a4d5bc18a3e2b
preethi19mit <preethi19mit@gmail.com>
2025-08-05 10:59:07 -0400

    new Queue

--- src/test/java/org/example/runners/TestRunner.java
+++ src/test/java/org/example/runners/TestRunner.java
@@ -1,4 +1,16 @@
 package org.example.runners;
 
-public class TestRunner {
-}
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+	@CucumberOptions(
+	plugin = {"pretty"},//, "html:target/report1.html"}, //reporting purpose
+		monochrome=true,  //console output color
+			//tags = "@tag or @persoanlLeave", //tags from feature file
+			features = {"src/test/resources/features"}, //location of feature files
+			glue= "stepdefinition") //location of step definition files
+
+
+	public class TestRunner extends AbstractTestNGCucumberTests{
+	}
