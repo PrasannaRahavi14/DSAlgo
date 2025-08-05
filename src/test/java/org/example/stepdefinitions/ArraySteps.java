fd8f3a7df9796451d52f9e0fb9078ac7c8965514
priyagunasekaran404 <priyagunasekaran404@gmail.com>
2025-08-04 23:10:20 -0400

    created step defintion and feature file for array page

---
+++ src/test/java/org/example/stepdefinitions/ArraySteps.java
@@ -0,0 +1,19 @@
+package org.example.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ArraySteps {
+    @Given("User is in Home page after successful sign-in")
+    public void userIsInHomePageAfterSuccessfulSignIn() {
+    }
+
+    @When("Click Get Started in the array or click on Data Structures dropdown to select Arrays")
+    public void clickGetStartedInTheArrayOrClickOnDataStructuresDropdownToSelectArrays() {
+    }
+
+    @Then("User should be navigated to the Array page")
+    public void userShouldBeNavigatedToTheArrayPage() {
+    }
+}
