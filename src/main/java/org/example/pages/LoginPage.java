5684fa0d836a91d81d24269141d347c009246ff4
preethi19mit <preethi19mit@gmail.com>
2025-08-16 17:40:56 -0400

    Login added

--- src/main/java/org/example/pages/LoginPage.java
+++ src/main/java/org/example/pages/LoginPage.java
@@ -1,34 +1,42 @@
 package org.example.pages;
 
-import org.example.utilities.BaseLogger;
+import org.example.utilities.ElementsUtil;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class LoginPage extends BaseLogger {
+import rst.pdfbox.layout.text.ReplacedWhitespace;
+
+public class LoginPage {
 
     private WebDriver driver;
+    private ElementsUtil elementutil;
 
     //1. By Locators :
     private By SignInLink = By.xpath("//a[text()='Sign in']");
     private By Username = By.xpath("//input[@name='username']");
     private By Password = By.xpath("//input[@name='password']");
     private By LoginBtn = By.cssSelector("input[value='Login']");
+    private By Alertmsg = By.id("id_username");
+    private By invalidmsg = By.xpath("//div[@role='alert']");
 
     //2. Constructor of the page class :
     public LoginPage(WebDriver driver)
     {
         this.driver=driver;
+        this.elementutil= new ElementsUtil(driver);
     }
 
     //3. page actions
     public void enterUserName(String un)
     {
-        driver.findElement(Username).sendKeys(un);
+        driver.findElement(Username).sendKeys("dummyuser");
     }
 
     public void enterPassword(String pwd)
     {
-        driver.findElement(Password).sendKeys(pwd);
+        driver.findElement(Password).sendKeys("numpy@123");
     }
 
     public  void clickOnLogin()
@@ -36,12 +44,71 @@ public class LoginPage extends BaseLogger {
         driver.findElement(LoginBtn).click();
     }
 
+    public void login()
+    {
+    	driver.findElement(SignInLink).click();
+    }
+    
     public void doLogin(String un, String pwd)
     {
-        log.info("Performing Login");
-        driver.findElement(Username).sendKeys(un);
-        driver.findElement(Password).sendKeys(pwd);
+        driver.findElement(Username).sendKeys("dummyuser");
+        driver.findElement(Password).sendKeys("numpy@123");
         driver.findElement(LoginBtn).click();
     }
+    
+    public void replacewithemptystring()
+    {
+    	driver.findElement(Username).sendKeys("");
+    	driver.findElement(Password).sendKeys("");
+    	driver.findElement(LoginBtn).click();
+    }
+    public void replacewithemptystringusrnme()
+    {
+    	driver.findElement(Username).sendKeys("");
+    	driver.findElement(LoginBtn).click();
+    
+    }
+    public void replacewithemptystringpwd(String un)
+    {
+    	driver.findElement(Username).sendKeys(un);
+    	driver.findElement(Password).sendKeys("");
+    	driver.findElement(LoginBtn).click();
+    }
+    
+    public void invalidusrnme(String pwd)
+    {
+    	driver.findElement(Username).sendKeys("hello");
+    	driver.findElement(Password).sendKeys(pwd);
+    }
+    
+    public void invalidpwd(String un)
+    {
+    	driver.findElement(Username).sendKeys(un);
+    	driver.findElement(Password).sendKeys("world");
+    	driver.findElement(LoginBtn).click();
+    }
+    
+    public void alert()
+    {
+    	driver.findElement(LoginBtn).click();
+    	
+    	WebElement field = driver.findElement(Alertmsg);
+    	boolean isRequired = Boolean.parseBoolean(field.getAttribute("required"));
+    	System.out.println("Is required? " + isRequired);
+    	
+    	Boolean valid = (Boolean) ((JavascriptExecutor) driver)
+    	        .executeScript("return arguments[0].checkValidity();", field);
 
+    	if (!valid) {
+    	    String validationMessage = (String) ((JavascriptExecutor) driver)
+    	        .executeScript("return arguments[0].validationMessage;", field);
+    	    System.out.println("Browser says: " + validationMessage);
+    	}
+    }
+    
+    public void Invalidmsg()
+    {
+    	 driver.findElement(invalidmsg);
+    }
+   
 }
---
+++ target/test-classes/features/login.feature
@@ -0,0 +1,35 @@
+Feature: User sigin to DSAlgo Portal
+
+Background:
+Given the user is on the Signin page
+
+
+@fail
+Scenario: Failed login due to empty username and password
+When the user clicks "Login" button 
+Then the error message should be displayed
+
+@fail1
+Scenario: Failed Login due to Username missing
+When the user clicks the "Login" button without entering username field
+Then the user should be displayed with error message "Please fill out this field"
+
+@fail2
+Scenario: Failed Login due to password missing
+When the user clicks the "login" button without entering password field
+Then the user should be displayed with error message "Please fill out this field"
+
+@fail3
+Scenario: Failed Login due to incorrect Username
+When the user clicks the "Login" button after the invalid Username is entered
+Then the error message "Invalid Username and Password" should be displayed
+
+@fail4
+Scenario: Failed Login due to incorrect password
+When the user clicks the "Login" button after the invalid Password is entered
+Then the error message "Invalid Username and Password" should be displayed
+
+@pass
+Scenario: Succesful login
+When the user clicks the Login button after the valid "username" and "Password" is entered
+Then the user will be directed to the Home Page with "You are logged in" message displayed
