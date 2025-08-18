02e59fa6a91df72211aff6a8cf140c70c0b1d878
priyagunasekaran404 <priyagunasekaran404@gmail.com>
2025-08-18 16:30:27 -0400

    Added ArrayPage class #3

--- src/test/java/org/example/stepdefinitions/ArraySteps.java
+++ src/test/java/org/example/stepdefinitions/ArraySteps.java
@@ -3,8 +3,25 @@ package org.example.stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.example.factory.DriverFactory;
+import org.example.pages.DataStructurePage;
+import org.example.pages.HomePage;
+import org.example.pages.LandingPage;
+import org.example.pages.LoginPage;
+import org.example.utilities.ConfigReader;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
 public class ArraySteps {
+
+    WebDriver driver = DriverFactory.getDriver();
+    LoginPage loginpage = new LoginPage(DriverFactory.getDriver());
+    LandingPage landingpage = new LandingPage(DriverFactory.getDriver());
+    HomePage homePage = new HomePage(DriverFactory.getDriver());
+
+    DataStructurePage dsp = new DataStructurePage(DriverFactory.getDriver());
+    String url = ConfigReader.getProperty("baseurl");
+
     @Given("User is in Home page after successful sign-in")
     public void userIsInHomePageAfterSuccessfulSignIn() {
     }
@@ -27,5 +44,16 @@ public class ArraySteps {
 
     @Then("User should be redirected to Arrays in Python page")
     public void userShouldBeRedirectedToArraysInPythonPage() {
+    }
+
+      @Given("User is in Array page after successful login using username {string} and password {string}")
+    public void userIsInArrayPageAfterSuccessfulLoginUsingUsernameAndPassword(String username, String password) {
+
+          driver.get(url);
+          String title_lp =  landingpage.getTitle();
+          Assert.assertEquals(title_lp, "Preparing for the Interviews");
+          landingpage.clickGetStartedBtn();
+          homePage.clickSignInLink();
+          loginpage.doLogin(username,password);
     }
 }
--- src/test/resources/features/Arrays.feature
+++ src/test/resources/features/Arrays.feature
@@ -1,11 +1,14 @@
 @Array
   Feature: Testing Array page
-    Background: User is in Array page
+    Background:
+    Given User is in Array page after successful login using username "Prasanna" and password "Welcome@01"
+
+      @array1
       Scenario: Verify if the user is able to navigate to Array page
         Given User is in Home page after successful sign-in
         When Click Get Started in the array or click on Data Structures dropdown to select Arrays
         Then User should be navigated to the Array page
-
+@array2
     Scenario: Verify if the user is able to navigate to Array page
       Given User is on the Array page
       When User clicks Arrays in Python hyperlink
