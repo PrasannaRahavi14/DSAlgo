ffb6dad0729a86e07ed9ea8156c34c0a3fc0a6d5
preethi19mit <preethi19mit@gmail.com>
2025-08-20 23:29:03 -0400

    graph

--- src/test/java/org/example/runners/TestRunner.java
+++ src/test/java/org/example/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/Graph.feature",
+        features = "src/test/resources/features",
         glue = {"org.example.stepdefinitions","org.example.hooks"},
         plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                            "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
