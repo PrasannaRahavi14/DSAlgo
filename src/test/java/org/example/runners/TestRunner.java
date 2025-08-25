517d8d2751d240c75393bfba55e4d0fda1796daf
PrasannaRahavi14 <Prasannarahavi14@gmail.com>
2025-08-25 19:41:31 -0400

    Update

--- src/test/java/org/example/runners/TestRunner.java
+++ src/test/java/org/example/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/Register.feature",
         glue = {"org.example.stepdefinitions","org.example.hooks"},
         plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                            "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
