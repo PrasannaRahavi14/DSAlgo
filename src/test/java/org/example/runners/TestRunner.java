eaf37706199f1c9ee208677a31d8d23171de7fd0
sindhuaccet07 <sindhuaccet07@gmail.com>
2025-08-26 11:42:41 -0400

    changed runner back to normal

--- src/test/java/org/example/runners/TestRunner.java
+++ src/test/java/org/example/runners/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/Register.feature",
+        features = "src/test/resources/features",
         glue = {"org.example.stepdefinitions","org.example.hooks"},
         plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                            "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
